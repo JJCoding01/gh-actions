9b0a8e0fde4c26947596314e10d47399e57cd41b
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-09-29 21:00:15 -0500

    Add sub method

--- actions/actions.py
+++ actions/actions.py
@@ -3,3 +3,7 @@ def add(*args):
     Return the sum of all arguments
     """
     return sum([arg for arg in args])
+
+def sub(*args):
+    """Return all arguments subtracted from each other"""
+    return args[0] + sum([-arg for arg in args[1:]])
