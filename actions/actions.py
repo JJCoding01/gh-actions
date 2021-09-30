b4d00e218eb5ce910cae5e4a2329e98101ae8a98
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2021-09-29 21:01:51 -0500

    Remove lint from actions

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,7 @@ def add(*args):
     """
     return sum([arg for arg in args])
 
+
 def sub(*args):
     """Return all arguments subtracted from each other"""
     return args[0] + sum([-arg for arg in args[1:]])
